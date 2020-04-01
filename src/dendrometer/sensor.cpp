abf11c8ec3d446a9fd1d4cc4b4311ea741a889df
Jason Manuel <jama.indo@hotmail.com>
2020-04-01 13:11:45 -0700

    Change the voltage to distance calculation for the rotation sensor
    We now use the same calculation as that in
    src/rotation_sensor/rotation_sensor.ino.

--- src/dendrometer/sensor.cpp
+++ src/dendrometer/sensor.cpp
@@ -1,6 +1,6 @@
-#include <Arduino.h>
-#include "config.h"
 #include "sensor.h"
+#include "config.h"
+#include <Arduino.h>
 
 extern float calibration_voltage;
 
@@ -10,17 +10,17 @@ int map_value_to_5000(int sensor_value) {
 }
 
 float read_sensor_voltage(void) {
-  #if DENDROMETER_SENSOR == mock
+#if DENDROMETER_SENSOR == mock
   return 0.5;
-  #elif DENDROMETER_SENSOR == rotation
+#elif DENDROMETER_SENSOR == rotation
   const int sensor_value = analogRead(DENROMETER_ROTATION_SENSOR_PIN);
   const int voltage = map_value_to_5000(sensor_value);
-  return voltage/1000.0;
-  #endif
+  return voltage / 1000.0;
+#endif
 }
 
 float distance_to_degrees(float distance) {
-  return distance/ROTATION_SENSOR_CIRCUMFERENCE*360;
+  return distance / ROTATION_SENSOR_CIRCUMFERENCE * 360;
 }
 
 // find what voltage corresponds to zero based on calibration
@@ -28,15 +28,18 @@ float find_voltage_for_zero() { return calibration_voltage; }
 
 // returns inches
 float voltage_to_distance(float voltage) {
-  #if DENDROMETER_SENSOR == mock
-  return voltage*8;
-  #elif DENDROMETER_SENSOR == rotation
+#if DENDROMETER_SENSOR == mock
+  return voltage * 8;
+#elif DENDROMETER_SENSOR == rotation
+  // TODO: I should not make this relative to a 'zero' voltage because we want
+  // to remember the rotation after the Arduino is powered off.
   const float voltage_for_zero = find_voltage_for_zero();
   float degrees;
-  degrees = (voltage - voltage_for_zero)/ROTATION_SENSOR_VOLTS_PER_DEGREE;
-  const float inches_per_mm = 1/25.4;
-  return degrees/360*2*PI*ROTATION_SENSOR_RADIUS*inches_per_mm;
-  #endif
+  degrees = (voltage - voltage_for_zero) / ROTATION_SENSOR_VOLTS_PER_DEGREE;
+  const float inches_per_mm = 1 / 25.4;
+  return // Distance in inches from rtheta, the equation for arclength
+      degrees * (13.0 / 16.0) * (1.0 / 2.0) * PI / 180.0;
+#endif
 }
 
 bool sensor_calibrate_wanted(void) {
