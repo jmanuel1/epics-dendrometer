df3205cdb13250323dd41a6f3922b2d8637de3ef
Jason Manuel <jama.indo@hotmail.com>
2020-04-02 23:58:55 -0700

    Move dendrometer.ino to src/dendrometer
    This change allows the dendrometer code to be built as an Arduino sketch.

--- src/dendrometer/dendrometer.ino
+++ src/dendrometer/dendrometer.ino
@@ -1,14 +1,12 @@
+#include "config.h"
+#include "sensor.h"
+#include "transmitter.h"
 #include <Arduino.h>
-#include "src/dendrometer/config.h"
-#include "src/dendrometer/sensor.h"
-#include "src/dendrometer/transmitter.h"
 
 float calibration_voltage = -1;
 
 /* Core logic */
-void calibrate(void) {
-  calibration_voltage = read_sensor_voltage();
-}
+void calibrate(void) { calibration_voltage = read_sensor_voltage(); }
 
 float calculate_distance() {
   float voltage = read_sensor_voltage();
@@ -22,9 +20,9 @@ void send_to_receiver(float distance) {
 }
 
 void setup() {
-  #if DENDROMETER_TRANSMITTER == mock
+#if DENDROMETER_TRANSMITTER == mock
   Serial.begin(9600);
-  #endif
+#endif
 }
 
 void loop() {
